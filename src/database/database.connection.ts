c89f3d4d4f58c7d073feb1af589ae556449fd073
Evelyn <evelynbandeiramesquita@gmail.com>
2023-09-16 12:59:27 -0300

    feat: database connection

---
+++ src/database/database.connection.ts
@@ -0,0 +1,14 @@
+import pg from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();
+const { Pool } = pg;
+const configDatabase: Object = {
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.mode === "prod" ? true : false,
+};
+
+const db = new Pool(configDatabase);
+
+export default db;
+
