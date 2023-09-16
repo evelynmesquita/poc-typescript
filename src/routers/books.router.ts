c9d5045915afafa269610fc9548ff8dc940e6bbb
Evelyn <evelynbandeiramesquita@gmail.com>
2023-09-16 17:07:27 -0300

    fix: routes

--- src/routers/books.router.ts
+++ src/routers/books.router.ts
@@ -5,7 +5,6 @@ import { bookSchema } from "../schemas/book.schema";
 
 const booksRouter = Router();
 booksRouter.get("/books", booksController.listBooks);
-booksRouter.get("/books/:book", booksController.listByBooks);
 booksRouter.post("/books", schemaValidation(bookSchema), booksController.createBook);
 booksRouter.delete("/books/relation/:id", booksController.deleteBookRelation);
 booksRouter.patch("/books/relation/:id", schemaValidation(bookSchema), booksController.editRelation);
